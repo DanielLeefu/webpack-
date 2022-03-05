da9cca8d8fd2ba47f080265007c686affdf1f9e1
DanielLeefu <you@2228429150@qq.com>
2022-03-05 21:16:10 +0800

    feat: add webpack plugin diff loader .md

--- src/tapable.js
+++ src/tapable.js
@@ -8,3 +8,17 @@ hook.tap('some name', () => {
 })
 // 触发 相当于点击click
 hook.call();
+
+// 简单实现SyncHooks 实际就是发布订阅
+class SyncHookMy  {
+  constructor () {
+    this.taps = [];
+  }
+  tap(name, fn) {
+    this.taps.push(fn);
+  }
+  call() {
+    this.taps.forEach(tap => tap())
+  }
+}
+
